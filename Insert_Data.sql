b97a621c6c9c1ddc0579681e9d3d50a40d4556b2
blopablito <pablo.infantes@ucsp.edu.pe>
2026-05-21 09:54:45 -0500

    feat: add initial data for mapas, continentes, and paises tables

--- Insert_Data.sql
+++ Insert_Data.sql
@@ -12,3 +12,24 @@ VALUES
     (1, 5, 1, 2),
     (2, 3, 2, 1),
     (3, 4, 1, 3);
+
+INSERT INTO mapas (nro_continentes, nro_paises) VALUES 
+(6, 42), 
+(4, 25), 
+(5, 30), 
+(7, 50), 
+(3, 15); 
+
+INSERT INTO continentes (nro_paises, mapa_id) VALUES 
+(12, 1), 
+(10, 1), 
+(8,  2), 
+(7,  2), 
+(15, 3); 
+
+INSERT INTO paises (nombre, ejercito, poblacion, economia, conquistado, continente_id) VALUES 
+('Argentina', 5000, 45000000, 3, FALSE, 1), 
+('Brasil', 8000, 210000000, 4, FALSE, 1),   
+('Canadá', 4000, 38000000, 5, FALSE, 2),    
+('Francia', 7000, 67000000, 5, TRUE, 3),    
+('Japón', 6000, 125000000, 5, FALSE, 4);   
