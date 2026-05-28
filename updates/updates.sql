498b679173e8076f30f9519f13efe320ddd07384
Jhoao Pereyra <jhoao.pereyra@ucsp.edu.pe>
2026-05-28 08:52:33 -0500

    feat: agregar consulta UPDATE para revisar si un pais ya esta conquistado

--- updates/updates.sql
+++ updates/updates.sql
@@ -3,3 +3,7 @@ UPDATE jugadores
 SET habilidad = habilidad + 3
 WHERE ejercito > 2000;
 
+--Marcar países con poca economía como conquistados
+UPDATE paises
+SET conquistado = TRUE
+WHERE economia < 4;
