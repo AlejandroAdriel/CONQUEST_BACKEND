4b78c5c568fdfab2696095364b68f1db50495a75
AlejandroAdriel <alejandroadrielq@gmail.com>
2026-05-28 00:57:58 -0500

    feat: reorganize

--- 02_dml_poblado_datos.sql
+++ 02_dml_poblado_datos.sql
@@ -12,21 +12,6 @@ INSERT INTO jugadores (usuario_id, habilidad, territorio, ejercito, economia, po
 (4, 18, 9, 3500, 9000, 28000),  
 (5, 12, 4, 1200, 4500, 9000);   
 
-INSERT INTO habilidades (nombre, descripcion, bonificacion_economia, bonificacion_tropa, costo) VALUES
-('Agricultura', 'Mejora la producción de recursos', 10, 0, 100),
-('Infantería', 'Aumenta la fuerza de las tropas de infantería', 0, 15, 200),
-('Comercio', 'Reduce costos en rutas comerciales', 15, 5, 150),
-('Ingeniería', 'Mejora defensas y construcciones', 5, 10, 250),
-('Diplomacia', 'Mejora relaciones y alianzas', 20, 0, 120);
-
-
-INSERT INTO arbol_habilidades (tipo, nro_habilidades, progresion, ramas) VALUES
-(1, 5, 1, 2),
-(2, 3, 2, 1),
-(3, 4, 1, 3),
-(1, 6, 2, 2), 
-(2, 5, 3, 2);
-
 INSERT INTO mapas (nro_continentes, nro_paises) VALUES 
 (6, 42), 
 (4, 25), 
@@ -48,19 +33,33 @@ INSERT INTO paises (nombre, ejercito, poblacion, economia, conquistado, continen
 ('Francia', 7000, 67000000, 5, TRUE, 3),    
 ('Japón', 6000, 125000000, 5, FALSE, 4);   
 
+INSERT INTO arbol_habilidades (tipo, nro_habilidades, progresion, ramas) VALUES
+(1, 5, 1, 2),
+(2, 3, 2, 1),
+(3, 4, 1, 3),
+(1, 6, 2, 2), 
+(2, 5, 3, 2);
+
+INSERT INTO habilidades (nombre, descripcion, bonificacion_economia, bonificacion_tropa, costo) VALUES
+('Agricultura', 'Mejora la producción de recursos', 10, 0, 100),
+('Infantería', 'Aumenta la fuerza de las tropas de infantería', 0, 15, 200),
+('Comercio', 'Reduce costos en rutas comerciales', 15, 5, 150),
+('Ingeniería', 'Mejora defensas y construcciones', 5, 10, 250),
+('Diplomacia', 'Mejora relaciones y alianzas', 20, 0, 120);
+
+INSERT INTO ejercitos_jugador (nro_tropas) VALUES 
+(1500), 
+(3000), 
+(500), 
+(3500), 
+(1200);
+
 INSERT INTO tropas (tipo, costo, ataque, defensa) VALUES 
 (1, 100, 15, 10), 
 (1, 150, 20, 25), 
 (2, 250, 35, 15), 
 (2, 400, 50, 30), 
 (3, 500, 60, 5);   
-
-INSERT INTO eventos (descripcion, bonificacion) VALUES
-('Tormenta de Arena: Caos en tierra firme', -3),
-('Corte repentino de suministros: Hambruna en el campamento', -5),
-('Lluvia intensa: Avance Lento', -4),
-('Alianza concretada: Liderazgo solido', 6),
-('Discurso inspirador: Alza de impetu', 5); 
 
 INSERT INTO infanterias (tropas_id, bono_defensa) VALUES
 (1,5),
@@ -83,19 +82,19 @@ INSERT INTO artillerias (tropas_id,bono_ataque,  bono_defensa) VALUES
 (5, 25, 2),
 (6, 40, 5);
 
-INSERT INTO ejercitos_jugador (nro_tropas) VALUES 
-(1500), 
-(3000), 
-(500), 
-(3500), 
-(1200);
-
 INSERT INTO tiempos (fecha_partida, velocidad, pausado) VALUES 
 ('2026-05-28 00:00:00', 1, FALSE),
 ('2026-05-28 12:30:00', 2, FALSE),
 ('2026-05-28 18:45:10', 1, TRUE),
 ('2026-05-29 02:15:00', 3, FALSE),
 ('2026-05-29 09:00:00', 1, TRUE);
+
+INSERT INTO eventos (descripcion, bonificacion) VALUES
+('Tormenta de Arena: Caos en tierra firme', -3),
+('Corte repentino de suministros: Hambruna en el campamento', -5),
+('Lluvia intensa: Avance Lento', -4),
+('Alianza concretada: Liderazgo solido', 6),
+('Discurso inspirador: Alza de impetu', 5); 
 
 INSERT INTO partidas (estado, fecha, tiempo) VALUES 
 (TRUE, '2026-05-28 00:00:00', 3600),
