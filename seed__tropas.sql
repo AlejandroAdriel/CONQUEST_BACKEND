473a99b6f2169e9e1b45ecdbeb08940bb9aa2d59
francisko41 <francisco.cucho@ucsp.edu.pe>
2026-06-14 09:48:01 -0500

    feat: INSERT tropas

--- seed__tropas.sql
+++ seed__tropas.sql
@@ -4,4 +4,15 @@ INSERT INTO tropas (tropa_id, nombre_tropa, costo_base, multiplicador_combate) V
 (3, 'Motorista de Asalto Cyber', 45, 1.5),
 (4, 'Nómada del Desierto', 60, 1.8),
 (5, 'Cañón de Plasma Pesado', 120, 3.0),
-(6, 'Meca de Asedio Goliath', 250, 4.0);
+(6, 'Meca de Asedio Goliath', 250, 4.0),
+(7, 'Nómada del Desierto', 60, 1.8),
+(8, 'Cazador en Monorrueda', 55, 1.4),
+(9, 'Jinete de Neodraco', 90, 2.2),
+(10, 'Flanqueador Veloz', 40, 1.3),
+
+-- Para Artillerías (IDs 11 al 15)
+(11, 'Cañón de Plasma Pesado', 120, 3.0),
+(12, 'Meca de Asedio Goliath', 250, 4.0),
+(13, 'Lanzamisiles Enjambre', 150, 3.2),
+(14, 'Mortero de Pulso EMP', 110, 2.5),
+(15, 'Batería de Riel Magnético', 300, 5.0);    
