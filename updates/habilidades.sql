a9984cf8f3b8757eafdd02b124ad26d5180157a0
AlejandroAdriel <alejandroadrielq@gmail.com>
2026-06-17 16:53:48 -0500

    feat: truncate habilidades table

--- updates/habilidades.sql
+++ updates/habilidades.sql
@@ -1,2 +1,6 @@
 -- 2026-06-01: Se agrega FK faltante en habilidades
-ALTER TABLE habilidades ADD COLUMN arbol_id INT REFERENCES arbol_habilidades(arbol_id);
+ALTER TABLE habilidades
+ADD COLUMN arbol_id INT REFERENCES arbol_habilidades (arbol_id);
+
+-- 2026-06-17: Necesario actualizar todas las habilidades previas por ello se limpiara toda la tabla 
+TRUNCATE TABLE habilidades CASCADE;
