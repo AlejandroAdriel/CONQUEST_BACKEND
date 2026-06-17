15b03de5629f1629466b463f05aa1fb40def5547
AlejandroAdriel <alejandroadrielq@gmail.com>
2026-06-17 15:16:32 -0500

    feat: rename columns and update constraints in usuarios table schema

---
+++ updates/usuarios.sql
@@ -0,0 +1,20 @@
+ALTER TABLE usuarios
+RENAME COLUMN contrasena TO password_hash;
+
+ALTER TABLE usuarios
+RENAME COLUMN creado TO fecha_registro;
+
+ALTER TABLE usuarios
+ALTER COLUMN pais TYPE VARCHAR(50),
+ALTER COLUMN pais
+SET
+    NOT NULL,
+ALTER COLUMN rango
+SET
+    NOT NULL,
+ALTER COLUMN fecha_registro TYPE TIMESTAMP
+WITH
+    TIME ZONE,
+ALTER COLUMN fecha_registro
+SET
+    NOT NULL;
