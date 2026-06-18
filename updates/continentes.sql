0274c92b0d68643f57dafcb7a22b895d478bb361
jppaulo1 <81366931+jppaulo1@users.noreply.github.com>
2026-06-18 00:15:23 -0500

    fix: alterar continentes en updates para adaptarse al frontend

--- updates/continentes.sql
+++ updates/continentes.sql
@@ -1,1 +1,13 @@
 
+ALTER TABLE continentes 
+DROP COLUMN nro_paises;
+
+ALTER TABLE continentes 
+ADD COLUMN nombre_continente VARCHAR(100) NOT NULL;
+
+ALTER TABLE continentes 
+DROP CONSTRAINT IF EXISTS continentes_mapa_id_fkey;
+
+ALTER TABLE continentes 
+ADD CONSTRAINT continentes_mapa_id_fkey 
+FOREIGN KEY (mapa_id) REFERENCES mapas (mapa_id) ON DELETE CASCADE;
