d49505d3e223d5d209550ed3fb9a5bd7132aeca5
jppaulo1 <81366931+jppaulo1@users.noreply.github.com>
2026-06-18 00:18:52 -0500

    fix: modificar caballerias para obtener la funcion delete on cascade

--- schema.sql
+++ schema.sql
@@ -138,12 +138,10 @@ CREATE TABLE infanterias (
     bono_defensa_trinchera NUMERIC(3,1) DEFAULT 0.0 NOT NULL CHECK (bono_defensa_trinchera >= 0.0)
 );
 
-CREATE TABLE
-    caballerias (
-        tropa_id INT PRIMARY KEY,
-        bono_ataque INT NOT NULL,
-        FOREIGN KEY (tropa_id) REFERENCES tropas (tropa_id)
-    );
+CREATE TABLE caballerias (
+    tropa_id INT PRIMARY KEY REFERENCES tropas(tropa_id) ON DELETE CASCADE,
+    bono_ataque_flanqueo NUMERIC(3,1) DEFAULT 0.0 NOT NULL CHECK (bono_ataque_flanqueo >= 0.0)
+);
 
 CREATE TABLE 
     artillerias (
