646f808db387dbc7aa2f25191fb5572ba91db619
AlejandroAdriel <alejandroadrielq@gmail.com>
2026-06-30 16:47:24 -0500

    feat: update habilidades table

--- updates/habilidades.sql
+++ updates/habilidades.sql
@@ -4,3 +4,19 @@ ADD COLUMN arbol_id INT REFERENCES arbol_habilidades (arbol_id);
 
 -- 2026-06-17: Necesario actualizar todas las habilidades previas por ello se limpiara toda la tabla 
 TRUNCATE TABLE habilidades CASCADE;
+
+ALTER TABLE habilidades
+    ALTER COLUMN costo SET DEFAULT 0,
+    ALTER COLUMN eje_x SET DEFAULT 0,
+    ALTER COLUMN eje_y SET DEFAULT 0;
+
+ALTER TABLE habilidades
+    ADD CONSTRAINT chk_habilidad_costo_positivo CHECK (costo >= 0);
+
+ALTER TABLE habilidades
+    ADD CONSTRAINT chk_categoria_valida CHECK (
+        categoria IN ('desarrollo', 'militar', 'especial')
+    );
+
+ALTER TABLE habilidades
+    ADD CONSTRAINT habilidades_nombre_unique UNIQUE (nombre);
